458d228af0faad112c5a84d0539256412e89fc3c
JeremyFriesenGitHub <jeremyfriesen@cmail.carleton.ca>
2025-02-18 21:00:01 -0500

    make landing page compatible with mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,19 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <h1 className="text-white text-4xl font-bold">
+      <Head>
+        <title>Jeremy Friesen</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] p-4 text-center">
+        <h1 className="text-white text-3xl md:text-5xl font-bold">
           Hello, World!
         </h1>
-        <Link href="https://github.com/JeremyFriesenGitHub/jeremy-friesen" className="text-white text-4xl font-bold underline mt-12">
-        See the repo progress here
+        <Link
+          href="https://github.com/JeremyFriesenGitHub/jeremy-friesen"
+          className="text-white text-lg md:text-2xl font-bold underline mt-6 md:mt-12"
+        >
+          See the repo progress here
         </Link>
       </main>
     </>
