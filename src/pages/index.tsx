c4709de6622c0ff5065500f85bccedd9154fac0d
JeremyFriesenGitHub <jeremyfriesen@cmail.carleton.ca>
2025-03-02 23:05:28 -0500

    update page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,36 +2,11 @@
 
 import Head from "next/head";
 import Link from "next/link";
-import { InteractiveGridPattern } from "../components/magicui/interactive-grid-pattern";
-import { useState, useEffect } from "react";
 import React from "react";
 import { Navbar } from "../components/custom/navbar";
+import { Boxes } from "~/components/ui/background-boxes";
 
 export default function Home() {
-  const [gridSize, setGridSize] = useState<[number, number]>([24, 24]); 
-
-  useEffect(() => {
-    function handleResize() {
-      if (typeof window !== 'undefined') {
-        setGridSize([
-          Math.ceil(window.innerWidth / 40),
-          Math.ceil(window.innerHeight / 40),
-        ]);
-      }
-    }
-
-    if (typeof window !== 'undefined') {
-      handleResize();
-    }
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      if (typeof window !== 'undefined') {
-        window.removeEventListener("resize", handleResize);
-      }
-    };
-  }, []);
 
   return (
     <>
@@ -39,32 +14,23 @@ export default function Home() {
         <title>Jeremy Friesen</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-  
-      <div className="relative min-h-screen bg-gradient-to-br from-[#A62B03] to-[#002244]">
-        
-        <Navbar />
-  
-        <InteractiveGridPattern
-          squares={gridSize}
-          width={40}
-          height={45}
-          className="w-full h-full"
-          squaresClassName="stroke-gray-400/10 transition-all duration-100 ease-in-out [&:not(:hover)]:duration-1000"
-        />
-  
 
-        <h1 className="text-white text-3xl md:text-5xl font-bold justify-center items-center flex h-full">
-          Hello, World!
-        </h1>
-  
-        <Link 
-          href="https://github.com/JeremyFriesenGitHub/jeremy-friesen" 
-          className="text-white text-lg md:text-2xl font-bold underline mt-6 md:mt-12 justify-center items-center flex h-full"
-        >
-          See the repo progress here
-        </Link>
-  
-      </div>
+      <main className="relative min-h-screen bg-[linear-gradient(60deg,#001489,#A23202,#0C2340)]">
+      <Navbar />
+      <Boxes />
+
+      <div className="relative flex flex-col items-center justify-center text-center p-8 text-white">
+          <h1 className="text-3xl md:text-5xl font-bold mt-8">
+            Hello, World!
+          </h1>
+          <Link className="text-2xl md:text-3xl underline font-bold mt-12" href="https://github.com/JeremyFriesenGitHub/jeremy-friesen">
+            See the repo progress here
+          </Link>
+      </div> 
+      </main>
+
+
+        
     </>
   );  
 }
