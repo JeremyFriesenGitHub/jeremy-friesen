1110c329538cc65b8b1289176fff536179ed0bb8
JeremyFriesenGitHub <jeremyfriesen@cmail.carleton.ca>
2025-03-03 20:23:29 -0500

    add text animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,17 @@ import Link from "next/link";
 import React from "react";
 import { Navbar } from "../components/custom/navbar";
 import { Boxes } from "~/components/ui/background-boxes";
+import { MorphingText } from "../components/magicui/morphing-text";
+import { TypingAnimation } from "../components/magicui/typing-animation";
+
+
+const texts = [
+  "Hello",
+  "World!",
+  "Welcome",
+  "Bonjour",
+];
+
 
 export default function Home() {
 
@@ -18,13 +29,14 @@ export default function Home() {
       <main className="relative min-h-screen bg-[linear-gradient(60deg,#001489,#A23202,#0C2340)]">
       <Navbar />
       <Boxes />
-
       <div className="relative flex flex-col items-center justify-center text-center p-8 text-white">
-          <h1 className="text-3xl md:text-5xl font-bold mt-8">
-            Hello, World!
-          </h1>
-          <Link className="text-2xl md:text-3xl underline font-bold mt-12" href="https://github.com/JeremyFriesenGitHub/jeremy-friesen">
-            See the repo progress here
+      <MorphingText texts={texts} className="text-3xl md:text-5xl font-bold mt-8" />
+
+          <Link
+            className="text-2xl md:text-3xl underline font-bold mt-12"
+            href="https://github.com/JeremyFriesenGitHub/jeremy-friesen"
+          >
+          <TypingAnimation>See the repo progess here</TypingAnimation>
           </Link>
       </div> 
       </main>
