898bf7334f19bd43e9564c8f441f15c43970b45c
JeremyFriesenGitHub <jeremyfriesen@cmail.carleton.ca>
2025-03-10 16:47:44 -0400

    add footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,10 @@ import { Navbar } from "../components/custom/navbar";
 import { Boxes } from "~/components/ui/background-boxes";
 import { MorphingText } from "../components/magicui/morphing-text";
 import { TypingAnimation } from "../components/magicui/typing-animation";
+import { VscAzure } from "react-icons/vsc";
+import { TbBrandNextjs } from "react-icons/tb";
+import { FaReact } from "react-icons/fa";
+import { SiTailwindcss, SiTypescript } from "react-icons/si";
 
 
 const texts = [
@@ -26,13 +30,32 @@ export default function Home() {
       </Head>
       
       <main className="relative flex flex-col min-h-screen w-full">
-        <div className="absolute inset-0 w-full h-full bg-[linear-gradient(90deg,#000000,#B11019,#000000)]"></div>
-        <Navbar />
-        <Boxes />
-        <MorphingText texts={texts} className="mt-8 relative flex flex-col items-center justify-center text-center p-8 text-white" />
-        <Link className="mt-12 underline relative flex flex-col items-center justify-center text-center p-8 text-white" href="https://github.com/JeremyFriesenGitHub/jeremy-friesen">
-          <TypingAnimation>See the repo progress here</TypingAnimation>
-        </Link>
+        <div className="absolute inset-0 w-full h-full bg-[linear-gradient(90deg,#000000,#B11019,#000000)]">
+          <Navbar />
+          <Boxes />
+          <MorphingText texts={texts} className="mt-8 relative flex flex-col items-center justify-center text-center p-8 text-white" />
+          <Link className="mt-12 underline relative flex flex-col items-center justify-center text-center p-8 text-white" href="https://github.com/JeremyFriesenGitHub/jeremy-friesen">
+            <TypingAnimation>See the repo progress here</TypingAnimation>
+          </Link>
+          <p className="relative mx-auto h-10 w-full text-lg sm:text-md md:text-xl lg:text-2xl leading-tight md:h-16 text-white flex flex-col items-center justify-center">Made with</p>
+          <div className="flex flex-wrap justify-center items-center mt-8 md:gap-10">
+            <Link className="relative mx-2 text-white flex flex-col items-center justify-center" href="https://azure.microsoft.com/en-ca">
+              <VscAzure size={30} className="sm:size-10 md:size-20" />
+            </Link>
+            <Link className="relative mx-2 text-white flex flex-col items-center justify-center" href="https://nextjs.org">
+              <TbBrandNextjs size={30} className="sm:size-10 md:size-20"/>
+            </Link>
+            <Link className="relative mx-2 text-white flex flex-col items-center justify-center" href="https://react.dev">
+              <FaReact size={30} className="sm:size-10 md:size-20"/>
+            </Link>
+            <Link className="relative mx-2 text-white flex flex-col items-center justify-center" href="https://tailwindcss.com">
+              <SiTailwindcss size={30} className="sm:size-10 md:size-20 "/>
+            </Link>
+            <Link className="relative mx-2 text-white flex flex-col items-center justify-center" href="https://www.typescriptlang.org">
+              <SiTypescript size={30} className="sm:size-10 md:size-20"/>
+            </Link>
+          </div>
+        </div>
       </main>
     </>
   );
