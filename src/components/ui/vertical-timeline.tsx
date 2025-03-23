efd6cf904daa1d8ad1589650f1afc8b3c1424016
JeremyFriesenGitHub <jeremyfriesen@cmail.carleton.ca>
2025-03-23 09:21:06 -0400

    bug fixes

--- src/components/ui/vertical-timeline.tsx
+++ src/components/ui/vertical-timeline.tsx
@@ -1,4 +1,3 @@
-import { CiLinkedin } from "react-icons/ci";
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import { LiaCanadianMapleLeaf } from "react-icons/lia";
