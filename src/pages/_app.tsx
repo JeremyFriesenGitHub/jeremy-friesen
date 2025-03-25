ba6eb46f2c34010ad59ef0cca37800df1a5a0405
JeremyFriesenGitHub <jeremyfriesen@cmail.carleton.ca>
2025-03-24 20:22:24 -0400

    change font to courier new

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,10 @@
-import { IBM_Plex_Mono } from "next/font/google";
 import { type AppType } from "next/app";
 
 import "~/styles/globals.css";
 
-const IBM = IBM_Plex_Mono({
-  subsets: ["latin"],
-  weight: ["400", "600", "700"],
-  variable: "--font-ibm-plex-mono",
-});
-
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className={IBM.className}>
+    <div style={{ fontFamily: "'Courier New', monospace" }}>
       <Component {...pageProps} />
     </div>
   );
