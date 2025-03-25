154a54a29ed40f86b9385601124ce50df505fa94
JeremyFriesenGitHub <jeremyfriesen@cmail.carleton.ca>
2025-03-24 20:22:49 -0400

    fix skills and section headers

--- src/components/custom/skills.tsx
+++ src/components/custom/skills.tsx
@@ -1,139 +1,98 @@
-import { FaPython } from "react-icons/fa";
-import { LinkPreview } from "~/components/ui/link-preview";
-import { SiPostgresql, SiTypescript } from "react-icons/si";
-import { FaJava } from "react-icons/fa";
+import { FaPython, FaJava, FaReact, FaDocker, FaGitAlt, FaFigma } from "react-icons/fa";
+import { SiPostgresql, SiTypescript, SiExpress, SiDrizzle, SiShadcnui, SiPandas, SiGeopandas, SiFolium, SiMermaid, SiScikitlearn, SiNetlify, SiGithubactions, SiJupyter, SiPycharm, SiIntellijidea, SiEclipseide, SiCytoscapedotjs, SiTailwindcss } from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
-import { SiExpress } from "react-icons/si";
 import { BiLogoFlask } from "react-icons/bi";
-import { SiTailwindcss} from "react-icons/si";
-import { FaReact } from "react-icons/fa";
-import { SiDrizzle } from "react-icons/si";
-import { SiShadcnui } from "react-icons/si";
-import { SiPandas } from "react-icons/si";
-import { SiGeopandas } from "react-icons/si";
-import { SiFolium } from "react-icons/si";
-import { SiMermaid } from "react-icons/si";
-import { SiScikitlearn } from "react-icons/si";
-import { VscAzure } from "react-icons/vsc";
-import { SiNetlify } from "react-icons/si";
-import { FaDocker } from "react-icons/fa";
-import { SiGithubactions } from "react-icons/si";
-import { FaGitAlt } from "react-icons/fa6";
-import { VscGithubAlt } from "react-icons/vsc";
-import { VscVscode } from "react-icons/vsc";
-import { SiJupyter } from "react-icons/si";
-import { SiPycharm } from "react-icons/si";
-import { SiIntellijidea } from "react-icons/si";
-import { SiEclipseide } from "react-icons/si";
+import { VscAzure, VscGithubAlt, VscVscode } from "react-icons/vsc";
+import { IoLogoCss3, IoLogoHtml5, IoLogoJavascript } from "react-icons/io5";
+import { LinkPreview } from "~/components/ui/link-preview";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "~/components/ui/card"
 
+const skillsData = [
+  {
+    title: "Languages",
+    skills: [
+      { icon: <FaPython />, url: "https://www.python.org" },
+      { icon: <SiPostgresql />, url: "https://www.postgresql.org" },
+      { icon: <SiTypescript />, url: "https://www.typescriptlang.org" },
+      { icon: <FaJava />, url: "https://www.java.com" },
+      { icon: <IoLogoHtml5 />, url: "https://www.w3schools.com/html/" },
+      { icon: <IoLogoCss3 />, url: "https://www.w3.org/Style/CSS/Overview.en.html" },
+      { icon: <IoLogoJavascript />, url: "https://www.javascript.com" }
+    ]
+  },
+  {
+    title: "Frameworks",
+    skills: [
+      { icon: <TbBrandNextjs />, url: "https://nextjs.org" },
+      { icon: <SiExpress />, url: "https://expressjs.com" },
+      { icon: <BiLogoFlask />, url: "https://flask.palletsprojects.com" },
+      { icon: <SiTailwindcss />, url: "https://tailwindcss.com" }
+    ]
+  },
+  {
+    title: "Libraries",
+    skills: [
+      { icon: <FaReact />, url: "https://react.dev" },
+      { icon: <SiDrizzle />, url: "https://orm.drizzle.team" },
+      { icon: <SiShadcnui />, url: "https://ui.shadcn.com" },
+      { icon: <SiPandas />, url: "https://pandas.pydata.org" },
+      { icon: <SiGeopandas />, url: "https://geopandas.org" },
+      { icon: <SiFolium />, url: "https://python-visualization.github.io/folium/latest/" },
+      { icon: <SiMermaid />, url: "https://mermaid.js.org" },
+      { icon: <SiScikitlearn />, url: "https://scikit-learn.org" },
+      { icon: <SiCytoscapedotjs />, url: "https://js.cytoscape.org" }
+    ]
+  },
+  {
+    title: "DevOps",
+    skills: [
+      { icon: <VscAzure />, url: "https://azure.microsoft.com" },
+      { icon: <SiNetlify />, url: "https://www.netlify.com" },
+      { icon: <FaDocker />, url: "https://www.docker.com" },
+      { icon: <SiGithubactions />, url: "https://github.com/features/actions" }
+    ]
+  },
+  {
+    title: "Tools",
+    skills: [
+      { icon: <FaGitAlt />, url: "https://git-scm.com" },
+      { icon: <VscGithubAlt />, url: "https://github.com" },
+      { icon: <VscVscode />, url: "https://code.visualstudio.com" },
+      { icon: <SiJupyter />, url: "https://jupyter.org" },
+      { icon: <SiPycharm />, url: "https://www.jetbrains.com/pycharm/" },
+      { icon: <SiIntellijidea />, url: "https://www.jetbrains.com/idea/" },
+      { icon: <SiEclipseide />, url: "https://eclipseide.org" },
+      { icon: <FaFigma />, url: "https://www.figma.com" }
+    ]
+  }
+];
+
+const colors = ["#A32B0A", "#426100", "#005E8A", "#705000"];
 
 export function Skills() {
-    return (   
-    <>
-         <p className="mt-20 items-center justify-center text-center text-white">Languages:</p><div className="mt-10 flex flex-wrap justify-center items-center gap-6 md:gap-10">
-            <LinkPreview className="relative text-white items-center justify-center" url="https://www.python.org">
-                <FaPython size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-
-            <LinkPreview className="relative text-white items-center justify-center" url="https://www.postgresql.org">
-                <SiPostgresql size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-
-            <LinkPreview className="relative text-white items-center justify-center" url="https://www.typescriptlang.org">
-                <SiTypescript size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-
-            <LinkPreview className="relative text-white items-center justify-center" url="https://www.java.com">
-                <FaJava size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-
-        </div>
-
-        <p className="mt-20 items-center justify-center text-center text-white">Frameworks:</p><div className="mt-10 flex flex-wrap justify-center items-center gap-6 md:gap-10">
-            <LinkPreview className="relative text-white items-center justify-center" url="https://nextjs.org">
-                <TbBrandNextjs size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://expressjs.com">
-                <SiExpress size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://flask.palletsprojects.com">
-                <BiLogoFlask size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://tailwindcss.com">
-                <SiTailwindcss size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-        </div>
-
-        
-        <p className="mt-20 items-center justify-center text-center text-white">Libraries:</p><div className="mt-10 flex flex-wrap justify-center items-center gap-6 md:gap-10">
-            <LinkPreview className="relative text-white items-center justify-center" url="https://react.dev">
-                <FaReact size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://orm.drizzle.team">
-                <SiDrizzle size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://ui.shadcn.com">
-                <SiShadcnui size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://pandas.pydata.org">
-                <SiPandas size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://geopandas.org">
-                <SiGeopandas size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://python-visualization.github.io/folium/latest/">
-                <SiFolium size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://mermaid.js.org">
-                <SiMermaid size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://scikit-learn.org">
-                <SiScikitlearn size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-        </div>
-
-        
-        <p className="mt-20 items-center justify-center text-center text-white">DevOps:</p><div className="mt-10 flex flex-wrap justify-center items-center gap-6 md:gap-10">
-            <LinkPreview className="relative text-white items-center justify-center" url="https://azure.microsoft.com">
-                <VscAzure size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://www.netlify.com">
-                <SiNetlify size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://www.docker.com">
-                <FaDocker size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://github.com/features/actions">
-                <SiGithubactions size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-        </div>
-
-        <p className="mt-20 items-center justify-center text-center text-white">Tools:</p><div className="mt-10 flex flex-wrap justify-center items-center gap-6 md:gap-10">
-            <LinkPreview className="relative text-white items-center justify-center" url="https://git-scm.com">
-                <FaGitAlt size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://github.com">
-                <VscGithubAlt size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://code.visualstudio.com">
-                <VscVscode size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://jupyter.org">
-                <SiJupyter size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://www.jetbrains.com/pycharm/">
-                <SiPycharm size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://www.jetbrains.com/idea/">
-                <SiIntellijidea size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-            <LinkPreview className="relative text-white items-center justify-center" url="https://eclipseide.org">
-                <SiEclipseide size={40} className="sm:size-10 md:size-20" />
-            </LinkPreview>{" "}
-        </div>
-
-
-
-    </>
+    return (
+      <div className="text-center text-white grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 p-6">
+        {skillsData.map((category, index) => (
+          <Card key={index} className="text-white shadow-md p-4" style={{ backgroundColor: colors[index % colors.length] }}>
+            <CardHeader>
+              <CardTitle className="text-xl font-semibold text-center">{category.title}</CardTitle>
+            </CardHeader>
+            <CardContent className="grid grid-cols-3 gap-6 p-4 justify-center items-center">
+              {category.skills.map((skill, idx) => (
+                <LinkPreview key={idx} className="flex items-center justify-center relative" url={skill.url}>
+                  <div className="text-white text-5xl sm:text-6xl md:text-7xl">{skill.icon}</div>
+                </LinkPreview>
+              ))}
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     );
 }
