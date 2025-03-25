9be7b4b48094ea2040fc44e54bf4a475fe5b24ba
JeremyFriesenGitHub <jeremyfriesen@cmail.carleton.ca>
2025-03-24 20:33:08 -0400

    fix

--- src/components/custom/skills.tsx
+++ src/components/custom/skills.tsx
@@ -8,8 +8,6 @@ import { LinkPreview } from "~/components/ui/link-preview";
 import {
     Card,
     CardContent,
-    CardDescription,
-    CardFooter,
     CardHeader,
     CardTitle,
   } from "~/components/ui/card"
